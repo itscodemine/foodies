5289e4ec063bed27ff40d405f5ae37720c79a195
Tatang Kuswandi <itskuswandi@gmail.com>
2026-01-30 21:59:29 +0700

    feat: update user profile screen

--- lib/ui/screens/profile_screen.dart
+++ lib/ui/screens/profile_screen.dart
@@ -2,6 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:foodies/models/user_model.dart';
 import 'package:foodies/services/auth_services.dart';
+import 'package:foodies/ui/screens/address_screen.dart';
+import 'package:foodies/ui/screens/favorite_screen.dart';
+import 'package:foodies/ui/screens/order_history_screen.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -39,44 +42,137 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profile'),
-        backgroundColor: Colors.green,
+        title: const Text('Profile', style: TextStyle(color: Colors.black)),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        iconTheme: const IconThemeData(color: Colors.black),
+        centerTitle: true,
       ),
       body: _isLoading
           ? const Center(child: CircularProgressIndicator())
           : _user == null
           ? const Center(child: Text('User not found.'))
-          : Padding(
+          : SingleChildScrollView(
               padding: const EdgeInsets.all(16.0),
               child: Column(
                 children: [
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundImage: _user!.imageUrl != null
-                        ? NetworkImage(_user!.imageUrl!)
-                        : null,
-                    child: _user!.imageUrl == null
-                        ? const Icon(Icons.person, size: 50)
-                        : null,
-                  ),
-                  const SizedBox(height: 20),
-                  Text(
-                    _user!.name,
-                    style: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.green[50],
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    _user!.email,
-                    style: const TextStyle(fontSize: 16, color: Colors.grey),
+                    child: Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 50,
+                          backgroundImage: _user!.imageUrl != null
+                              ? NetworkImage(_user!.imageUrl!)
+                              : null,
+                          child: _user!.imageUrl == null
+                              ? const Icon(Icons.person, size: 50)
+                              : null,
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          _user!.name,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          _user!.email,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                   const SizedBox(height: 30),
                   ElevatedButton(
                     onPressed: () {},
-                    child: const Text('Edit Profile'),
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.green,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 40,
+                        vertical: 15,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    ),
+                    child: const Text(
+                      'Edit Profile',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
+                  const SizedBox(height: 30),
+
+                  ListTile(
+                    leading: const Icon(
+                      Icons.location_on_outlined,
+                      color: Colors.black,
+                    ),
+                    title: const Text(
+                      'My Addresses',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const AddressScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(
+                      Icons.history_outlined,
+                      color: Colors.black,
+                    ),
+                    title: const Text(
+                      'Order History',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const OrderHistoryScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                  const Divider(),
+                  ListTile(
+                    leading: const Icon(
+                      Icons.favorite_border,
+                      color: Colors.black,
+                    ),
+                    title: const Text(
+                      'Favorites',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const FavoriteScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                  const Divider(),
                 ],
               ),
             ),
